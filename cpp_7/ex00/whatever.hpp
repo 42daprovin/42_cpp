be3ae71e386c27defa964f25485900615d950ef5
daprovin <daprovin@student.42.fr>
2021-10-23 00:26:34 +0200

    cpp_7 ex00

---
+++ cpp_7/ex00/whatever.hpp
@@ -0,0 +1,21 @@
+template<typename T>
+void	swap(T & a, T & b)
+{
+	T	tmp;
+
+	tmp = b;
+	b = a;
+	a = tmp;
+}
+
+template<typename T>
+T const		& min(T const & a, T const & b)
+{
+	return (a < b ? a : b);
+}
+
+template<typename T>
+T const		& max(T const & a, T const & b)
+{
+	return (a > b ? a : b);
+}
